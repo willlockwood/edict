865a06daa00261bebea4e1c822e55ec52ba9236d
willlockwood <wml248@nyu.edu>
2019-10-30 10:29:45 -0400

    add activity, theme, and resource files to support new splash screen

--- app/src/main/java/com/willlockwood/edict/activity/SplashActivity.kt
+++ app/src/main/java/com/willlockwood/edict/activity/SplashActivity.kt
@@ -1,4 +1,17 @@
 package com.willlockwood.edict.activity
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+
+        finish()
+    }
+
 }
